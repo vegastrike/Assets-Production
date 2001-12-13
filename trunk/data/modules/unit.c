1c92f00bc3696745247840cc1d11e351d747cb94
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-13 04:07:47 +0000

    first call_unit
    2001-12-12 Alexander Rawass <alexannika@users.sourceforge.net>
    
    	* added file src/cmd/script/script_call_unit.cpp

---
+++ trunk/data/modules/unit.c
@@ -0,0 +1,21 @@
+module unit {
+  // support for the _unit callback class
+
+  void print_unitlist(){
+    float ship_nr=0.0;
+
+    while(ship_nr<7.0){
+      object unit=_unit.getUnit(ship_nr);
+
+      object pos=_unit.getPosition(unit);
+
+      float i=_olist.at(pos,0.0);
+      float j=_olist.at(pos,1.0);
+      float k=_olist.at(pos,2.0);
+
+      _io.PrintFloats(:s1="Unit Nr, i,j,k "; ship_nr,i,j,k);
+      ship_nr=ship_nr+1.0;
+    }
+  };
+
+}
