ed238838266520e769becf001cbf7e155860209b
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-14 02:04:36 +0000

    strings for the interpreter
    2001-12-13 Alexander Rawass <alexannika@users.sourceforge.net>
    
    	* added strings to c_alike.yacc
    	* added string callbacks src/cmd/script/script_call_string.cpp
    	* made Unit remember the Flightgroup & nr
    	* testd_unit1.mission now prints out also the fg id

---
+++ modules/string.c
@@ -0,0 +1,11 @@
+module string {
+
+  void loop(){
+    object str1="Hello";
+    object str2=" World";
+
+    _string.print(str1);
+    _string.print(str2);
+
+  };
+}
--- modules/unit.c
+++ modules/unit.c
@@ -4,16 +4,18 @@ module unit {
   void print_unitlist(){
     float ship_nr=0.0;
 
-    while(ship_nr<7.0){
+    while(ship_nr<5.0){
       object unit=_unit.getUnit(ship_nr);
 
       object pos=_unit.getPosition(unit);
+      object fgid=_unit.getFgId(unit);
 
       float i=_olist.at(pos,0.0);
       float j=_olist.at(pos,1.0);
       float k=_olist.at(pos,2.0);
 
-      _io.PrintFloats(:s1="Unit Nr, i,j,k "; ship_nr,i,j,k);
+      _string.print(fgid);
+      _io.PrintFloats(:s1=" Unit Nr, i,j,k "; ship_nr,i,j,k);
       ship_nr=ship_nr+1.0;
     }
   };
