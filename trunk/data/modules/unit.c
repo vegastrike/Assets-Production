d0e73fae0f7f73260983235fdebc54a56c6c7263
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-15 19:16:16 +0000

    scripts can launch ships
    2001-12-15 Alexander Rawass <alexannika@users.sourceforge.net>
    
    	* added _std.isNull function to test for null objects
    	* added callback 'launch' to launch new ships from script

--- trunk/data/modules/unit.c
+++ trunk/data/modules/unit.c
@@ -1,11 +1,28 @@
 module unit {
   // support for the _unit callback class
 
+  float lasttime;
+
+  void initgame(){
+    lasttime=0.0;
+  };
+
+  void launch_test(){
+    float time=_std.getGameTime();
+
+    if((time-lasttime)>10.0){
+      _unit.launch("omega","confed","hornet","default",4.0, 8000.0, 0.0-100.0, 100.0);
+      _unit.launch("teta","kirathi","dagger","default",4.0 , 8000.0, 1000.0, 0.0-500.0);
+      lasttime=time;
+    }
+  };
+
   void print_unitlist(){
     float ship_nr=0.0;
 
-    while(ship_nr<5.0){
-      object unit=_unit.getUnit(ship_nr);
+    object unit=_unit.getUnit(ship_nr);;
+
+    while(!_std.isNull(unit)){
 
       object pos=_unit.getPosition(unit);
       object fgid=_unit.getFgId(unit);
@@ -16,7 +33,15 @@ module unit {
 
       _string.print(fgid);
       _io.PrintFloats(:s1=" Unit Nr, i,j,k "; ship_nr,i,j,k);
+
+      object blah="bbl";
+
+      _string.print(blah);
+
+      //      _io.printf("fgid=%s  ship_nr=%f pos: %f %f %f\n",blah,ship_nr,i,j,k);
+
       ship_nr=ship_nr+1.0;
+      unit=_unit.getUnit(ship_nr);
     }
   };
 
