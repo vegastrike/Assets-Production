ce2f357ad83dca463fad465e60f79b084ce415a6
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-16 06:33:56 +0000

    earth patrol

--- modules/earth_patrol_rnd_attack2.c
+++ modules/earth_patrol_rnd_attack2.c
@@ -1,4 +1,4 @@
-module earth_patrol_rnd_attack1 {
+module earth_patrol_rnd_attack2 {
   import random;
   import faction_ships;
   import launch;
@@ -6,24 +6,37 @@ module earth_patrol_rnd_attack1 {
 
   float lasttime;
   float waittime;
-  object aera_ships;
 
   void initgame(){
     lasttime=0.0;
     waittime=random.random(10.0,30.0);
     faction_ships.make_ships_list();
-    aera_ships=faction_ships.aera();
   };
 
   void launch_new_ships(){
-    object typename=faction_ships.getRandomShipType(aera_ships);
+    float side=_std.Rnd();
+    _io.PrintFloats(:s1="launching new wave side="; side);
+    
+    object ship_list;
+    object faction_name;
+
+    if(side>=0.5){
+      ship_list=faction_ships.confed();
+      faction_name="confed";
+    }
+    else{
+      ship_list=faction_ships.aera();
+      faction_name="aera";
+    }
+
+    object typename=faction_ships.getRandomShipType(ship_list);
 
     float nr_ships=random.random(2.0,4.0);
 
     object player=_unit.getPlayer();
     object player_pos=_unit.getPosition(player);
 
-    launch.launch_wave_around_area("fgname","aera",typename,"default",nr_ships,500.0,2000.0,player_pos);
+    launch.launch_wave_around_area("fgname",faction_name,typename,"default",nr_ships,500.0,2000.0,player_pos);
   };
 
   void loop(){
--- modules/launch.c
+++ modules/launch.c
@@ -2,12 +2,31 @@ module launch {
 
   import random;
 
+  void launch_wave_around_area(object fgname,object faction,object type,object ai,float nr_ships,float r1,float r2,object pos){
+    
+
+    float x=_olist.at(pos,0.0)+(random.random(r1,r2)*random.randomsign());
+    float y=_olist.at(pos,1.0)+(random.random(r1,r2)*random.randomsign());
+    float z=_olist.at(pos,2.0)+(random.random(r1,r2)*random.randomsign());
+    //    float y=_olist.at(pos,1.0)+random.random(0.0,radius);
+    //float z=_olist.at(pos,2.0)+random.random(0.0,radius);
+
+    _io.PrintFloats(:s1="launching around area x,y,z,nr_ships"; x,y,z,nr_ships);
+    _string.print(faction);
+    _string.print(type);
+
+    _unit.launch(fgname,faction,type,ai,nr_ships,x,y,z);
+  };
+
+
   void launch_wave_in_area(object fgname,object faction,object type,object ai,float nr_ships,float radius,object pos){
     
 
-    float x=_olist.at(pos,0.0)+random.random(0.0,radius);
-    float y=_olist.at(pos,1.0)+random.random(0.0,radius);
-    float z=_olist.at(pos,2.0)+random.random(0.0,radius);
+    float x=_olist.at(pos,0.0)+random.random((0.0-radius)/2.0,radius/2.0);
+    float y=_olist.at(pos,1.0)+random.random((0.0-radius)/2.0,radius/2.0);
+    float z=_olist.at(pos,2.0)+random.random((0.0-radius)/2.0,radius/2.0);
+    //    float y=_olist.at(pos,1.0)+random.random(0.0,radius);
+    //float z=_olist.at(pos,2.0)+random.random(0.0,radius);
 
     _io.PrintFloats(:s1="launching at area x,y,z,nr_ships"; x,y,z,nr_ships);
     _string.print(faction);
