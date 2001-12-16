95c826e55593ee5a0176af83e9719a5c239a2a3f
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-16 06:33:56 +0000

    earth patrol

--- trunk/data/modules/random.c
+++ trunk/data/modules/random.c
@@ -1,5 +1,16 @@
 module random {
 
+  float randomsign(){
+    float r=_std.Rnd();
+    
+    if(r>5.0){
+      return 1.0;
+    }
+    else{
+      return (0.0-1.0);
+    }
+  };
+
   float random(float r1,float r2){
     float r=_std.Rnd();
 
