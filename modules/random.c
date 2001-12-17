5051f9abab247fe3785f3ce968b5dd9f5d3bf20c
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-17 04:20:48 +0000

    director, saving of variables
    2001-12-16 Alexander Rawass <alexannika@users.sourceforge.net>
    
    	* added src/cmd/script/director.cpp
    	* at end-of-game, the variables are serialized to disk
    	* add _string.equal, _std.equal

--- modules/random.c
+++ modules/random.c
@@ -13,15 +13,12 @@ module random {
 
   float random(float r1,float r2){
     float r=_std.Rnd();
-
     float range=r2-r1;
 
     r=r*range;
-
     r=r1+r;
 
-    _io.PrintFloats(:s1="random.random"; r1,r2,r);
-
+    //    _io.PrintFloats(:s1="random.random"; r1,r2,r);
     return r;
   };
 }
