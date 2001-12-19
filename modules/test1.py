daf31c7cdb0bb495b51e77ac3e2edcd68ecbc503
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-19 01:18:22 +0000

    datatype int, python support
    2001-12-18 Alexander Rawass <alexannika@users.sourceforge.net>
    
    	* datatype int implemented (untested)
    	* added basic python support (SCREAM!)

---
+++ modules/test1.py
@@ -0,0 +1,13 @@
+import os
+import sys
+# import time
+
+class VSdirector:
+    def __init(self):
+        print init
+        
+    def gameloop(self):
+        print "I am heavily annoyed"
+        
+director=VSdirector()
+director.gameloop()
