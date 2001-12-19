fc5a2da603c3c54c72d6f1a07d502b0e0f87606f
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-19 21:36:20 +0000

    int's working, printf
    2001-12-19 Alexander Rawass <alexannika@users.sourceforge.net>
    
    	* added _io.printf
    	* datatype int working

--- modules/launch.c
+++ modules/launch.c
@@ -8,29 +8,20 @@ module launch {
     float x=_olist.at(pos,0.0)+(random.random(r1,r2)*random.randomsign());
     float y=_olist.at(pos,1.0)+(random.random(r1,r2)*random.randomsign());
     float z=_olist.at(pos,2.0)+(random.random(r1,r2)*random.randomsign());
-    //    float y=_olist.at(pos,1.0)+random.random(0.0,radius);
-    //float z=_olist.at(pos,2.0)+random.random(0.0,radius);
 
-    _io.PrintFloats(:s1="launching around area x,y,z,nr_ships"; x,y,z,nr_ships);
-    _string.print(faction);
-    _string.print(type);
+     _io.printf("launching %f ships type=%s faction=%s around area [%f,%f,%f]\n",nr_ships,type,faction,x,y,z);
 
     _unit.launch(fgname,faction,type,ai,nr_ships,x,y,z);
   };
 
 
   void launch_wave_in_area(object fgname,object faction,object type,object ai,float nr_ships,float radius,object pos){
-    
 
     float x=_olist.at(pos,0.0)+random.random((0.0-radius)/2.0,radius/2.0);
     float y=_olist.at(pos,1.0)+random.random((0.0-radius)/2.0,radius/2.0);
     float z=_olist.at(pos,2.0)+random.random((0.0-radius)/2.0,radius/2.0);
-    //    float y=_olist.at(pos,1.0)+random.random(0.0,radius);
-    //float z=_olist.at(pos,2.0)+random.random(0.0,radius);
 
-    _io.PrintFloats(:s1="launching at area x,y,z,nr_ships"; x,y,z,nr_ships);
-    _string.print(faction);
-    _string.print(type);
+    _io.printf("launching %f ships type=%s faction=%s in area [%f,%f,%f]\n",nr_ships,type,faction,x,y,z);
 
     _io.message("game","all","launching new wave of fighters:");
     _io.message("game","all",type);
