783e4f39e31c9aeb5347f24ab23c122d1676097a
alexannika <alexannika@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-29 01:56:36 +0000

    first ai works with new scripting
    2001-12-28 Alexander Rawass <alexannika@users.sourceforge.net>
    
    	* the first ai works now with new scripting
    	* improved vdu/messages

--- trunk/data/modules/ai_switching1.c
+++ trunk/data/modules/ai_switching1.c
@@ -1,4 +1,4 @@
-module ai_test {
+module ai_switching1 {
 
   import random;
   import vec3;
@@ -10,6 +10,7 @@ module ai_test {
   class float wait_time;
   class float gametime;
   class bool did_it;
+  class int mode;
 
   void randomMoveTo(){
     object player=_unit.getPlayer();
@@ -18,51 +19,44 @@ module ai_test {
     float r1=500.0;
     float r2=2000.0;
 
-    float x=_olist.at(ppos,0)+(random.random(r1,r2)*random.randomsign());
-    float y=_olist.at(ppos,1)+(random.random(r1,r2)*random.randomsign());
-    float z=_olist.at(ppos,2)+(random.random(r1,r2)*random.randomsign());
+    float x=9000.0;
+    float y=3000.0;
+    float z=0.0-5000.0;
 
     object myvec=vec3.new(x,y,z);
     object fgid=_unit.getFgId(my_unit);
     object outstr=_string.new();
-
-    _io.printf("moving %s to random %f %f %f\n",fgid,x,y,z);
-    _io.sprintf(outstr,"moving %s to random %f %f %f\n",fgid,x,y,z);
-    _io.message("game","all",outstr);
 
     object pos=_unit.getPosition(my_unit);
 
     float x=_olist.at(pos,0);
     float y=_olist.at(pos,1);
     float z=_olist.at(pos,2);
-
-    object fgid=_unit.getFgId(my_unit);
-
-    _io.printf("pos: %f %f %f fgid=%s\n",x,y,x,fgid);
-
-    object new_order=_order.newMoveTo(myvec,true,1);
-
-    _order.print(my_order);
-    _order.print(new_order);
-
+    
+    object new_order;
+    _io.printf("mode=%d\n",mode);
+    if(mode==0){
+      new_order=_order.newMoveTo(myvec,true,1);
+      _io.sprintf(outstr,"moving %s to  %f %f %f",fgid,x,y,z);
+    }
+    else{
+      new_order=_order.newAggressiveAI("default.agg.xml","default.int.xml");
+      _io.sprintf(outstr,"being aggressive");
+    }
     _order.enqueueOrder(my_order,new_order);
-  };
 
-  void aggrAI(){
-    object order=_order.newAggressiveAI("default","default");
-  };
+    _io.message("game","all",outstr);
 
-  void moveTo(object target_pos,bool afterburn, int nr_switchbacks){
-    object order=_order.newMoveTo(target_pos,afterburn,nr_switchbacks);
   };
 
   void initai(){
     rnd_num=_std.Rnd();
 
-    wait_time=random.random(5.0,20.0);
+    wait_time=5.0;
     gametime=_std.getGameTime();
     _io.printf("ai init: rnd_num=%f  wait=%f\n",rnd_num,wait_time);
     did_it=false;
+    mode=1;
   };
 
   void executeai(){
@@ -76,9 +70,15 @@ module ai_test {
 
     float newgametime=_std.getGameTime();
     if((newgametime-gametime) > wait_time){
+      if(mode==1){
+	mode=0;
+      }
+      else{
+	mode=mode+1;
+      }
       randomMoveTo();
       gametime=newgametime;
-      did_it=true;
+      //      did_it=true;
     }
     
   };
--- trunk/data/modules/ai_test2.c
+++ trunk/data/modules/ai_test2.c
@@ -1,5 +1,6 @@
-module ai_test {
+module ai_test2 {
 
+  // demonstrates the bug/feature in Orders::MoveTo
   import random;
   import vec3;
 
@@ -12,54 +13,27 @@ module ai_test {
   class bool did_it;
 
   void randomMoveTo(){
-    object player=_unit.getPlayer();
-    object ppos=_unit.getPosition(player);
-    
-    float r1=500.0;
-    float r2=2000.0;
-
-    float x=_olist.at(ppos,0)+(random.random(r1,r2)*random.randomsign());
-    float y=_olist.at(ppos,1)+(random.random(r1,r2)*random.randomsign());
-    float z=_olist.at(ppos,2)+(random.random(r1,r2)*random.randomsign());
+    float x=9000.0;
+    float y=2000.0;
+    float z=100.0; // set z to values >1000.0 to see the bug
 
     object myvec=vec3.new(x,y,z);
     object fgid=_unit.getFgId(my_unit);
     object outstr=_string.new();
 
     _io.printf("moving %s to random %f %f %f\n",fgid,x,y,z);
-    _io.sprintf(outstr,"moving %s to random %f %f %f\n",fgid,x,y,z);
+    _io.sprintf(outstr,"moving %s to %f %f %f",fgid,x,y,z);
     _io.message("game","all",outstr);
-
-    object pos=_unit.getPosition(my_unit);
-
-    float x=_olist.at(pos,0);
-    float y=_olist.at(pos,1);
-    float z=_olist.at(pos,2);
-
-    object fgid=_unit.getFgId(my_unit);
-
-    _io.printf("pos: %f %f %f fgid=%s\n",x,y,x,fgid);
 
     object new_order=_order.newMoveTo(myvec,true,1);
 
-    _order.print(my_order);
-    _order.print(new_order);
-
     _order.enqueueOrder(my_order,new_order);
-  };
-
-  void aggrAI(){
-    object order=_order.newAggressiveAI("default","default");
-  };
-
-  void moveTo(object target_pos,bool afterburn, int nr_switchbacks){
-    object order=_order.newMoveTo(target_pos,afterburn,nr_switchbacks);
   };
 
   void initai(){
     rnd_num=_std.Rnd();
 
-    wait_time=random.random(5.0,20.0);
+    wait_time=10.0;
     gametime=_std.getGameTime();
     _io.printf("ai init: rnd_num=%f  wait=%f\n",rnd_num,wait_time);
     did_it=false;
