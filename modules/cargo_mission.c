ee587fae4a0837d73f7603c38d00520d3e56410f
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-03-28 10:32:41 +0000

    fixed some bugs with hard mission

--- modules/cargo_mission.c
+++ modules/cargo_mission.c
@@ -1,4 +1,5 @@
 module cargo_mission {
+  import ai_stationary;
 	object faction;
 	object destination;
 	object basecontainer;
@@ -11,6 +12,7 @@ module cargo_mission {
 	import random;
 	import launch;
 	import faction_ships;
+	bool capship;
 	void sys (object currentsystem, int sysaway) {
 	  if (sysaway<=0) {
 	    destination=currentsystem;
@@ -26,7 +28,8 @@ module cargo_mission {
 	  }
 	};
 	
-	void init (int factionname, int numsystemsaway, int cargoquantity, int missiondifficulty, float distance_from_base, float creds) {
+	void init (int factionname, int numsystemsaway, int cargoquantity, int missiondifficulty, float distance_from_base, float creds, bool launchoncapship) {
+	  capship= launchoncapship;
 	  faction_ships.init();
 	  faction=faction_ships.intToFaction(factionname);
 	  arrived=false;
@@ -44,38 +47,58 @@ module cargo_mission {
 	  object list=_unit.getRandCargo(quantity);
 	  int tempquantity=quantity;
 	  cargoname=_olist.at(list,0);
-	  quantity = _unit.addCargo(you,cargoname        ,_olist.at(list,1),_olist.at(list,2),_olist.at(list,3),_olist.at(list,4),_olist.at(list,5));  //ADD CARGO HERE
-	  if (tempquantity>0) {
-	    cred=cred *_std.Float(quantity)/_std.Float(tempquantity);
+	  if (!_std.isNull(you)) {
+	    quantity = _unit.addCargo(you,cargoname        ,_olist.at(list,1),_olist.at(list,2),_olist.at(list,3),_olist.at(list,4),_olist.at(list,5));  //ADD CARGO HERE
+	    if (tempquantity>0) {
+	      cred=cred *_std.Float(quantity)/_std.Float(tempquantity);
+	    }
+	    
+	    object str = _string.new();
+	    object name = _unit.getName (you);
+	    _io.sprintf(str,"Good Day, %s",name);
+	    _io.message (0,"game","all",str);
+	    _io.sprintf(str,"Go to system %s and give the",destination);
+	    _io.message (1,"game","all",str);
+	    _io.sprintf(str,"cargo to a unit from our faction: %s",faction);
+	    _io.message (2,"game","all",str);
+	    _io.sprintf(str,"%d of the %s cargo",quantity,cargoname);
+	    _io.message (3,"game","all",str);
+	    _string.delete(str);
+	    _olist.delete(list);
+	  } else {
+	    _std.terminateMission (false);
 	  }
-
-	  object str = _string.new();
-	  _io.sprintf(str,"Go to system %s and give the",destination);
-	  _io.message (0,"game","all",str);
-	  _io.sprintf(str,"cargo to a unit from our faction: %s",faction);
-	  _io.message (1,"game","all",str);
-	  _io.sprintf(str,"%d of the %s cargo",quantity,cargoname);
-	  _io.message (2,"game","all",str);
-	  _string.delete(str);
-	  _olist.delete(list);
 	};
 	void takeCargoAndTerminate (object you) {
 	  int removenum=_unit.removeCargo(you,cargoname,quantity,true);
 	  if ((removenum==quantity)||(quantity==0)) {
+	    _io.message (0,"game","all","Excellent work pilot.");
+	    _io.message (0,"game","all","You have been rewarded for your effort as agreed.");
+	    _io.message (0,"game","all","Your contribution to the war effort will be remembered.");
 	    _unit.addCredits(cred);
 	    _std.terminateMission(true);
 	  } else {
-	    if (difficulty<=1) {
+	    _io.message (0,"game","all","You did not follow through on your end of the deal.");
+
+	    if (difficulty<1) {
+	      _io.message (0,"game","all","Your pay will be reduced");
+	      _io.message (0,"game","all","And we will consider if we will accept you on future missions.");
 	      float addcred=(_std.Float(removenum)/_std.Float((quantity*(1+difficulty))))*cred;
 	      _unit.addCredits(addcred);
 	    } else {
+	      _io.message (0,"game","all","You will not be paid!");
 	      if (difficulty>=2) {
+		_io.message (0,"game","all","And your idiocy will be punished.");
+		_io.message (0,"game","all","You had better run for what little life you have left.");
 		int i=0;
 		object un;
+
+
 		while (i<difficulty) {
 		  un=faction_ships.getRandomFighter(faction);
-		  un=launch.launch_wave_around_unit("shadow", faction, un, "default", 1, 1000.0,you);
-		  _unit.setTarget(un,you);
+		  object newunit=launch.launch_wave_around_unit("shadow", faction, un, "default", 1, 1000.0,you);
+		  _unit.setTarget(newunit,you);
+		  i=i+1;
 		}
 	      }
 	    }
@@ -102,8 +125,32 @@ module cargo_mission {
 	      arrived=true;
 	      object newship=faction_ships.getRandomCapitol(faction);
 	      int randint=random.randomint(0,50);
-	      object newunit=launch.launch_wave_around_significant("Base",faction,newship,"_ai_stationary",1,5000.0,randint);
-	      basecontainer=_unit.getContainer(newunit);
+	      object significant = unit.getSignificant (randint);
+	      if (_std.isNull (significant)) {
+		significant =_unit.getPlayer();
+	      }else {
+		object fgname = _unit.getFgName (significant);
+		while (!((_unit.isPlanet(significant))||(_string.equal(fgname,"Base")))&&(randint<1024)) {
+		  _string.delete (fgname);
+		  randint=randint+1;
+		  significant =unit.getSignificant(randint);
+		  fgname = _unit.getFgName (significant);
+		}
+	      }
+	      if (_std.isNull(significant)) {
+		arrived=false;
+	      }else {
+		if (capship) {
+		  significant=launch.launch_wave_around_unit("Base",faction,newship,"_ai_stationary",1,5000.0,significant);
+		}
+		object str = _string.new();
+		object name = _unit.getName (significant);
+		_io.sprintf(str,"You must drop your cargo off with the %s unit",name);
+		_io.message (0,"game","all",str);
+		_string.delete(str);
+		
+		basecontainer=_unit.getContainer(significant);
+	      }
 	    }
 	    _string.delete (sysfil);
 	  }
