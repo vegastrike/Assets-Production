a2c820cf7214249c319a81f295cffeaacbfa235c
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-04-01 12:40:35 +0000

    added a nice helper function

--- trunk/data/modules/attack_jumppoint.c
+++ trunk/data/modules/attack_jumppoint.c
@@ -1,35 +1,32 @@
 module attack_jumppoint {
   import ai_stationary;
   import universe;
+  import go_somewhere_significant;
   object youcontainer;
-	object faction;
-	object destination;
-	object basecontainer;
-	object cargoname;
-	int arrived;
+  object faction;
+  
 
-	float distfrombase;
-	float escdist;
-	int quantity;
-	float cred;
-	import random;
-	import launch;
-	import faction_ships;
-	int ship_check_count;
+  float escdist;
+  int quantity;
+  float cred;
+  import random;
+  import launch;
+  import faction_ships;
+  import go_somewhere_significant;
+  int ship_check_count;
 
   
 	void init (int factionname, int numsystemsaway, int enemyquantity, float distance_from_base, float escape_distance, float creds) {
 	  ship_check_count=0;
 	  faction_ships.init();
+
 	  faction=faction_ships.intToFaction(factionname);
 	  escdist = escape_distance;
-	  arrived=0;
 	  cred=creds;
-	  distfrombase=distance_from_base;
 
-	  object mysys=_std.getSystemFile();
 	  quantity=enemyquantity;
-	  object sysfile = _std.getSystemFile();
+
+	  
 	  object you=_unit.getPlayer();
 	  youcontainer=_unit.getContainer (you);
 
@@ -43,7 +40,8 @@ module attack_jumppoint {
 	  object str = _string.new();
 	  _io.sprintf(str,"Good Day, %s. Your mission is as follows:",name);
 	  _io.message (0,"game","all",str);
-	  destination=universe.getAdjacentSystem(sysfile,numsystemsaway);
+	  go_somewhere_significant.init(you,numsystemsaway,true,distance_from_base);
+
 	  _io.sprintf(str,"And there eliminate any %s starships at a point.",faction);
 	  _io.message (2,"game","all",str);
 	  _string.delete(str);
@@ -57,7 +55,8 @@ module attack_jumppoint {
 
 	bool NoEnemiesInArea (object jp) {
 	  object cur = _std.getSystemFile();
-	  if (!_string.equal(destination,cur)) {
+	  object tmpdst = go_somewhere_significant.DestinationSystem();
+	  if (!_string.equal(tmpdst,cur)) {
 	    return false;
 	  }
 	  object un= _unit.getUnit (ship_check_count);
@@ -78,66 +77,38 @@ module attack_jumppoint {
 	};
 	void GenerateEnemies (object jp,object you) {
 	  int count=0;
+	  object str = _string.new();
+	  _io.sprintf(str,"Eliminate all %s ships here.",faction);
+	  _io.message (0,"game","all",str);
+	  _string.delete(str);
+	  
 	  while (count<quantity) {
 	    object randtype = faction_ships.getRandomFighter(faction);
 	    object launched = launch.launch_wave_around_unit ("Shadow",faction,randtype,"default",1,4500.0,jp);
 	    _unit.setTarget (launched,you);//make 'em attack you
 	    count = count+1;
 	  }
+	  quantity=0;
     	};
 	void loop () {
-	  if (arrived==2) {
+	  go_somewhere_significant.loop();
+	  if (go_somewhere_significant.HaveArrived()) {
 	    object you=_unit.getUnitFromContainer(youcontainer);
 	    if (_std.isNull (you)) {
 	      _std.terminateMission(false);
 	    }else {
-	      object base = _unit.getUnitFromContainer (basecontainer);
+	      object base = go_somewhere_significant.SignificantUnit();
 	      if (_std.isNull(base)) {
 		SuccessMission(you);
 	      }else {
+		if (quantity>0) {
+		  GenerateEnemies (base,you);
+		}
 		if (NoEnemiesInArea (base)) {
 		  SuccessMission(you);
 		}
 	      }
 	    }
 	  }
-
-	  if (arrived==1) {
-	    object base=_unit.getUnitFromContainer(basecontainer);
-	    object you=_unit.getUnitFromContainer(youcontainer);
-	    if (_std.isNull(base)||_std.isNull(you)) {
-	      _std.terminateMission(false);
-	      return;
-	    }
-	    float dist=_unit.getDistance(base,you);
-	    if (dist<=distfrombase) {
-	      arrived=2;
-	      GenerateEnemies (base,you);
-	    }
-	  } else if (arrived==0) {
-	    object sysfil = _std.getSystemFile();
-	    if (_string.equal (sysfil,destination)) {
-	      arrived=1;
-	      object newship=faction_ships.getRandomCapitol(faction);
-	      int randint=random.randomint(0,50);
-	      object significant = unit.getSignificant (randint,false);
-	      if (_std.isNull (significant)) {
-		significant =_unit.getPlayer();
-	      }
-	      if (_std.isNull(significant)) {
-		arrived=0;
-	      }else {
-		object newun=significant;
-		object str = _string.new();
-		object name = _unit.getName (newun);
-		_io.sprintf(str,"You must visit the %s, and eliminate all %s ships there.",name,faction);
-		_io.message (0,"game","all",str);
-		_string.delete(str);
-       		basecontainer=_unit.getContainer(significant);
-	      }
-	    }
-	    _string.delete (sysfil);
-	  }
-
 	};
 }
