c88bac093807eb7cb1a2524c031d5f5ad4300ceb
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-04-09 04:24:48 +0000

    added capship mission

---
+++ trunk/data/modules/capship_mission.c
@@ -0,0 +1,11 @@
+module capship_mission {
+	float time;
+	int protectors;
+	void init (int numprotectors, float betweentime) {
+		time=betweentime;
+		protectors=numprotectors;
+	};
+
+	void loop () {
+	};
+}
