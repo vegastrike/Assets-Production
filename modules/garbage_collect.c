1852a0e41a688cd0418d73d141f0c84b12a567ca
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-04-09 07:51:19 +0000

    finalized our exploration mission

--- modules/garbage_collect.c
+++ modules/garbage_collect.c
@@ -7,17 +7,36 @@ module garbage_collect {
     invalid_distance = invaliddis;
     last_ship=0;
   };
-   void Loop() {
+   void loop() {
     object un= _unit.getUnit (last_ship);
     if (_std.isNull (un)) {
       last_ship=0;
     }else {
-      object player_unit=_unit.getPlayer();
-      if (!std.isNull(player_unit)) {
-	if (_unit.getDistance(un,player_unit)>invalid_distance&&(!_unit.isSignificant(un))&&(!_unit.isSun(un))) {
-	  last_ship=last_ship-1;
-	  _unit.removeFromGame(un);
+      if ((!_unit.isSignificant (un))&&(!_unit.isSun(un))) {
+	float dist=2.0*invalid_distance;
+	int j=0;
+	bool die=false;
+	object player_unit;
+	while (dist>=invalid_distance){
+	  player_unit=_unit.getPlayerX(j);
+	  if (!_std.isNull(player_unit)) {
+	    dist = _unit.getDistance (player_unit,un);
+	  }else {
+	    dist=0.0;
+	    if (j!=0) {
+	      die=true;
+	    }
+	  }
+	  j=j+1;
 	}	  
+	if (die) {
+	  last_ship=last_ship-1;
+	  object name = _unit.getName(un);
+	  _unit.removeFromGame(un);
+	  
+	  _io.printf ("garbage collecting %s",name);
+	  _string.delete (name);
+	}
       }
       last_ship=last_ship+1;
     }
--- modules/trading.c
+++ modules/trading.c
@@ -4,18 +4,17 @@ module trading {
   int last_ship;
   float price_instability;
   void init(){
-    lasttime=0.0;
     last_ship=0;
     price_instability=0.01;
     quantity=4;
   };
   void SetPriceInstability(float inst) {
-    price_instability=inst
-  }
+    price_instability=inst;
+  };
   void SetMaxQuantity (int quant) {
     quantity=quant;
-  }
-  void trade_cargo(){
+  };
+  void loop(){
     int incdec_ships=random.randomint(1,2);
     int quant = random.randomint (1,quantity);
     object un = _unit.getUnit (last_ship);
@@ -23,7 +22,7 @@ module trading {
       last_ship=0;
     } else {
       object player = _unit.getPlayer();
-      if (player!=un) {
+      if (!_unit.equal(player,un)) {
 	if (incdec_ships==1) {
 	  _unit.incrementCargo(un,1-(quant*price_instability),quant);
 	}else {
