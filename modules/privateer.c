cb398d63fff14416bad8f9ed7a30284ae98914ba
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-04-18 08:30:03 +0000

    another one bites the dust

--- modules/privateer.c
+++ modules/privateer.c
@@ -2,8 +2,10 @@ module privateer {
   import trading;
   import random_encounters;
   import garbage_collect;
+  import difficulty;
   bool garbage;
-  void init (float sigdis, float detectiondis, float gendis, int minships, int genships, float fighterprob, float capprob, float garbage_collect_dist) {//negative garbage collect dist disables that feature
+  void init (float sigdis, float detectiondis, float gendis, int minships, int genships, float fighterprob, float capprob, float credits_to_maximize_difficulty, float garbage_collect_dist) {//negative garbage collect dist disables that feature
+    difficulty.init (credits_to_maximize_difficulty);
     random_encounters.init (sigdis, detectiondis, gendis, minships,genships,fighterprob,capprob);
     garbage=false;
     if (garbage_collect_dist>0.0) {
@@ -13,6 +15,7 @@ module privateer {
     trading.init();
   };
   void loop() {
+    difficulty.loop();
     trading.loop();
     random_encounters.loop();
     if (garbage) {
