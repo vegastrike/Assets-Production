ea259cc1ba5ecdebe2e0fef96a8357ac9c043bff
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-06-08 05:11:03 +0000

    added some difficulty things...now there is a cred difficutly

--- trunk/data/modules/difficulty.c
+++ trunk/data/modules/difficulty.c
@@ -3,7 +3,22 @@ module difficulty {
   object saved_data;
   int playeriterator;
   float credsToMax;
-  void init(float creditsToMaximizeDifficulty) {
+  float cached_cred_difficulty;
+  float cred_ratio;
+  float getCredDifficulty() {
+    return getPlayerCredDifficulty (_unit.getPlayer());
+  };
+  float getPlayerCredDifficulty (object playa) {
+    float ret=0.0;
+    if (!_std.isNull(playa)) {
+      object temp =  _unit.getSaveData (playa,313374420.0);
+      if (_olist.size(temp)>1) {
+	ret = _olist.at (temp,1);
+      }
+    }
+    return ret;
+  };
+  void init(float creditsToMaximizeDifficulty, float cred_diff, float cred_diff_ratio) {
     credsToMax = creditsToMaximizeDifficulty;
     int whichplayer=0;
     object player=_unit.getPlayerX(0);
@@ -12,16 +27,27 @@ module difficulty {
     saved_data = _olist.new();
     creds=_olist.new();
     float diff = _std.getDifficulty();
+    cred_ratio = cred_diff_ratio;
+    cached_cred_difficulty = cred_diff;
     while (!_std.isNull(player)) {
       object temp = _unit.getSaveData (player,313374420.0);
       float mycred = _unit.getCredits (player);
       _olist.push_back(creds,mycred);
       if (_olist.size(temp)==0) {
 	_olist.push_back(temp,diff);
+	_olist.push_back(temp,cached_cred_difficulty);
       } else {
 	float saveddiff = _olist.at (temp,0);
 	if (saveddiff>diff) {
 	  diff = saveddiff;
+	}
+	if (_olist.size(temp)>1) {
+	  float savedcred = _olist.at(temp,1);
+	  if (savedcred>cached_cred_difficulty) {
+	    cached_cred_difficulty=savedcred;
+	  }
+	}else {
+	  _olist.push_back (temp,cached_cred_difficulty);
 	}
       }
       _olist.push_back(saved_data,temp);
@@ -39,12 +65,15 @@ module difficulty {
 	if (newcreds>oldcreds) {
 	  object save = _olist.at (saved_data,playeriterator);
        	  float difficulty = _olist.at (save,0);
+
 	  difficulty=difficulty+((newcreds-oldcreds)/credsToMax);
 	  if (difficulty>1.0){
 	    difficulty=1.0;
 	  }
 	  _olist.set(save,0,difficulty);
 	  _std.setDifficulty(difficulty);
+	  cached_cred_difficulty = cached_cred_difficulty+cred_ratio*(newcreds-oldcreds);
+	  _olist.set (save,1,cached_cred_difficulty);
 	}
 	_olist.set(creds,playeriterator,newcreds);
       }
