83a1324bec2e5720a6dabf0fa0ad988eb96ad3a0
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-06-08 05:11:03 +0000

    added some difficulty things...now there is a cred difficutly

--- modules/privateer.c
+++ modules/privateer.c
@@ -4,8 +4,8 @@ module privateer {
   import garbage_collect;
   import difficulty;
   bool garbage;
-  void init (float sigdis, float detectiondis, float gendis, int minships, int genships, float fighterprob, float enemyprob, float capprob, float credits_to_maximize_difficulty, float garbage_collect_dist, float capdist) {//negative garbage collect dist disables that feature
-    difficulty.init (credits_to_maximize_difficulty);
+  void init (float sigdis, float detectiondis, float gendis, int minships, int genships, float fighterprob, float enemyprob, float capprob, float credits_to_maximize_difficulty, float enemy_credits_begin, float enemy_credits_earned,float garbage_collect_dist, float capdist) {//negative garbage collect dist disables that feature
+    difficulty.init (credits_to_maximize_difficulty,enemy_credits_begin, enemy_credits_earned);
     random_encounters.init (sigdis, detectiondis, gendis, minships,genships,fighterprob,capprob,enemyprob,capdist);
     garbage=false;
     if (garbage_collect_dist>0.0) {
