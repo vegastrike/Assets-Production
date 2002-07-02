b079da2580ee8d0a6b5366da53afe38b95e16330
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-02 05:56:06 +0000

    printed hello

--- trunk/data/printhello.py
+++ trunk/data/printhello.py
@@ -2,8 +2,10 @@ import AI
 import sys
 class MyAI(AI.PythonAI):
     def Execute(self):
+#        AI.PythonAI.Execute(self);
         sys.stdout.write('h')
         return ''
 hi1 = MyAI()
+hi1.IncRef()
 print 'AI creation successful'
 hi1 = 0
