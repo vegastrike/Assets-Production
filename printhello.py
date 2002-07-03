74909da2e54c4b0b5a4c441af9df4f4205c23662
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-03 02:58:09 +0000

    fixed excp

--- printhello.py
+++ printhello.py
@@ -2,10 +2,11 @@ import AI
 import sys
 class MyAI(AI.PythonAI):
     def Execute(self):
-#        AI.PythonAI.Execute(self);
+        AI.PythonAI.Execute(self);
         sys.stdout.write('h')
         return ''
 hi1 = MyAI()
-hi1.IncRef()
+
 print 'AI creation successful'
 hi1 = 0
+#: 1.7; previous revision: 1.6
