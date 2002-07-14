50f10c9f61b12af18fe71502ff636cd41f78cb6d
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-14 05:21:54 +0000

    director

---
+++ trunk/data/modules/stub/director.py
@@ -0,0 +1,17 @@
+
+class Mission:
+    def Pickle(self):
+        print 'pickling'
+        return ''
+    def UnPickle(self,s):
+        print 'unpickling'
+    def Execute(self):
+        print 'execute'
+def putSaveData (whichplayer, key, num, val):
+    print 'putting save data'
+def getSaveDataLength (whichplayer, key):
+    return 1
+def getSaveData (whichplayer, key,num):
+    return 1024.5
+def pushSaveData (whichplayer,key,val):
+    print 'pushing save data'
