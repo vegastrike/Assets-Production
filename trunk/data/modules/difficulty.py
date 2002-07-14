0d8b4b1245f6eddfd5698f14b885bc42b2e1a32d
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-14 06:46:56 +0000

    completely redid it!

--- trunk/data/modules/difficulty.py
+++ trunk/data/modules/difficulty.py
@@ -1,69 +1,48 @@
 import sys
 import VS
 import pickle
+import Director
+_key="31337ness"
 class difficulty:
-  class pickil:
-    diff=0
-    def __init__(self,val=0):
-      self.diff=val
-  
-  creds=(0,)
-  playeriterator=0
+  diff=()
+  creds=()
   credsToMax=1
-  diffcls=pickil(0)
+  def SetDiff(diff):
+    if (diff>VS.GetDifficulty()):
+      VS.SetDifficulty(diff)
   
-  def SetDiff(self,val):
-    self.diffcls.diff=val
-    VS.SetDifficulty(val)
-  
-  def Pickle (self):
-    return pickle.dumps(self.diffcls)
-  
-  def UnPickle (self,fil):
-    self.diffcls=pickle.loads(fil)
-    VS.SetDifficulty(self.diffcls.diff)
-  
+  def __init__(self,credsMax):
+    self.credsToMax=credsMax
+    un=VS.getPlayerX(0)
+    i=0
+    while (un):
+      newdiff=0
+      if (Director.getSaveDataLength(i,_key)):
+        newdiff=Director.getSaveData(i,_key,0)
+        self.diff+=(newdiff,)
+      else:
+        newdiff=VS.GetDifficulty()
+        self.diff+=(newdiff,)
+        Director.pushSaveData(i,_key,newdiff)
+      SetDiff(newdiff)
+      self.creds+=(un.getCredits(),)
+      i+=1
+      un=VS.getPlayerX(i)
+      
   def usingDifficulty (self):
     return (VS.GetDifficulty()!=1.0)
   
   def getPlayerDifficulty (self,playa):
-    return self.diffcls.diff
+    return self.diff[playa]
   
-  def __init__(self,creditsToMaximizeDifficulty):
-    self.credsToMax = creditsToMaximizeDifficulty
-    whichplayer=0
-    player=VS.getPlayerX(0)
-    i=0
-    self.creds=()
-    diff = VS.GetDifficulty()
-    while (player):
-      temp = VS.getSaveData (player,"31337ness") #???
-      mycred = player.getCredits ()
-      self.creds=creds+(mycred,)
-      if (len(temp)==0):
-	temp=(diff,)
-      else:
-	saveddiff = temp[0]
-	if (saveddiff>diff):
-	  diff = saveddiff
-      i+=1
-      player=VS.getPlayerX(i)
-    VS.SetDifficulty(diff)    
+  def Execute(self):
+    for i in range(len(self.creds)):
+      un=VS.getPlayerX(i)
+      newcreds=un.getCredits()
+      if (self.creds[i]!=newcreds):
+        if (self.creds[i]>newcreds):
+          newdiff=((newcreds-self.creds)/self.credsToMax)
+          Director.putSaveData(i,_key,0,newdiff)
+          SetDiff(newdiff)
+        self.creds[i]=newcreds
   
-  def Execute (self):
-    player = VS.getPlayerX(playeriterator)
-    if (player):
-      oldcreds = creds[playeriterator]
-      newcreds = player.getCredits ()
-      if (newcreds!=oldcreds):
-	if (newcreds>oldcreds):
-          difficulty=self.diffcls.diff
-	  difficulty+=((newcreds-oldcreds)/credsToMax)/len(creds)
-	  if (difficulty>0.999999):
-	    difficulty=0.999999
-          SetDiff(difficulty)
-	creds[playeriterator]=newcreds
-      playeriterator+=1
-    else:
-      playeriterator=0
-
