a40388a69e71e478dcf35e5adea0a42e2dbb404a
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-14 03:36:40 +0000

    fixed pickle stuff...

--- modules/difficulty.py
+++ modules/difficulty.py
@@ -17,21 +17,17 @@ class difficulty:
     VS.SetDifficulty(val)
   
   def Pickle (self):
-    return pickle.dumps(self.pickil)
+    return pickle.dumps(self.diffcls)
   
-  def UnPickle (self):
-    return pickle.loads(self.pickil)
+  def UnPickle (self,fil):
+    self.diffcls=pickle.loads(fil)
+    VS.SetDifficulty(self.diffcls.diff)
   
   def usingDifficulty (self):
     return (VS.GetDifficulty()!=1.0)
   
   def getPlayerDifficulty (self,playa):
-    ret=0.0
-    if (playa):
-      temp =  VS.getSaveData (playa,"31337ness") #???
-      if (len(temp)>0):
-	ret = temp[0]
-    return ret
+    return self.diffcls.diff
   
   def __init__(self,creditsToMaximizeDifficulty):
     self.credsToMax = creditsToMaximizeDifficulty
@@ -55,21 +51,16 @@ class difficulty:
     VS.SetDifficulty(diff)    
   
   def Execute (self):
-    player = _unit.getPlayerX(playeriterator)
+    player = VS.getPlayerX(playeriterator)
     if (player):
       oldcreds = creds[playeriterator]
-      newcreds = _unit.getCredits (player)
+      newcreds = player.getCredits ()
       if (newcreds!=oldcreds):
 	if (newcreds>oldcreds):
-	  save = VS.getSaveData (player,"31337ness") #???
-	  if (len(save)>0):
-	    difficulty = save[0]
-	  else:
-	    difficulty = VS.GetDifficulty()
-	  difficulty+=((newcreds-oldcreds)/credsToMax)
-	  if (difficulty>0.99999):
-	    difficulty=0.99999
-          SetDiff(difficulty)
+          difficulty=self.diffcls.diff
+	  difficulty+=((newcreds-oldcreds)/credsToMax)/len(creds)
+	  if (difficulty>0.999999):
+	    difficulty=0.999999
           SetDiff(difficulty)
 	creds[playeriterator]=newcreds
       playeriterator+=1
