8cd6b5fdbe070e617ffa459446fa2a3c0d0738cd
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-22 20:30:24 +0000

    briefing stub

---
+++ trunk/data/modules/stub/Briefing.py
@@ -0,0 +1,23 @@
+# 82 "briefing_wrapper.cpp"
+def addShip(self,name,faction,vec): 
+   print "addShip" 
+   return 0
+def removeShip(self,ship): 
+   print "removeShip"
+def enqueueOrder(self,ship,dest,time): 
+   print "enqueueOrder"
+def replaceOrder(self,ship,dest,time): 
+   print "replaceOrder"
+def getShipPosition(self,ship): 
+   print "getShipPosition" 
+   return (50,75,100)
+def setShipPosition(self,ship,vec): 
+   print "setShipPosition"
+def terminate(self): 
+   print "terminate"
+def setCamPosition(self,pos): 
+   print "setCamPosition"
+def setCamOrientation(self,p,q,r): 
+   print "setCamOrientation"
+def setCloak(self,ship,amount): 
+   print "setCloak"
