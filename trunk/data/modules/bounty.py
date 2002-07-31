9a87a3a6981756d0404f9d23f2794960e640170c
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-31 04:27:06 +0000

    fixed some self. bugs and SetTarget

--- trunk/data/modules/bounty.py
+++ trunk/data/modules/bounty.py
@@ -12,7 +12,7 @@ import VS
 class bounty (Director.Mission):
 
 	def __init__ (self,minnumsystemsaway, maxnumsystemsaway, creds, run_away, shipdifficulty, tempfaction):
-	  Director.Mission.__init__ ()
+	  Director.Mission.__init__ (self)
 	  self.newship=""
 	  self.mplay="all"
 	  self.istarget=0
@@ -30,10 +30,10 @@ class bounty (Director.Mission):
 	  self.significant=VS.Unit()
 	  self.adjsys=go_to_adjacent_systems (self.you,random.randrange(minnumsystemsaway,maxnumsystemsaway+1))
 	  self.mplay=universe.getMessagePlayer(self.you)
-	  if (you):
-	    VS.IOmessage (0,"bounty mission",self.mplay,"Good Day, %s. Your mission is as follows:" % (you.getName()))
+	  if (self.you):
+	    VS.IOmessage (0,"bounty mission",self.mplay,"Good Day, %s. Your mission is as follows:" % (self.you.getName()))
 	    self.adjsys.Print("You should start in the system named %s","Then jump to %s","Finally, jump to %s, your final destination","bounty mission",1)
-	    VS.IOmessage (2,"bounty mission",self.mplay,"Once there, you must destroy a %s unit." % (faction))
+	    VS.IOmessage (2,"bounty mission",self.mplay,"Once there, you must destroy a %s unit." % (self.faction))
 	    VS.IOmessage (3,"bounty mission",self.mplay,"You will then %.2f credits as your reward (if you survive)." % (self.cred))
 	    VS.IOmessage (4,"bounty mission",self.mplay,"#00ff00Good luck!")
 	  else:
@@ -55,7 +55,7 @@ class bounty (Director.Mission):
 	    print "lose bounty mission"
 	    VS.terminateMission(0)
 	  
-	def loop (self):
+	def Execute (self):
 	  isSig=0
 	  if (self.you.isNull()):
 	    self.Lose (1)
@@ -67,11 +67,8 @@ class bounty (Director.Mission):
 		if (curun==self.enemy):
 		  self.enemy.setTarget(self.you)
 	      self.curiter+=1
-	    if (you.isNull()):
-	      self.Lose(1)
-	      return
 	    if (self.enemy.isNull()):
-	      self.Win(you,1)
+	      self.Win(self.you,1)
 	      return
 	  elif (self.arrived==2):
 	    if (VS.getSystemFile()==self.adjsys.DestinationSystem()):
@@ -79,10 +76,10 @@ class bounty (Director.Mission):
 	    else:
 	      VS.ResetTimeCompression()
 	    if (self.you.isNull()):
-	      Lose(1)
+	      self.Lose(1)
 	      return
 	    if (self.enemy.isNull()):
-	      Win(you,1)
+	      self.Win(self.you,1)
 	      return
 	  elif (self.arrived==1):
 	    significant=self.significant
@@ -90,13 +87,13 @@ class bounty (Director.Mission):
 	      print "sig null"
 	      VS.terminateMission(0)
 	    else:
-	      if (you.getSignificantDistance(significant)<10000.0):
+	      if (self.you.getSignificantDistance(significant)<10000.0):
 		if (self.newship==""):
 		  self.newship=faction_ships.getRandomFighter(faction)
 		self.enemy=launch.launch_wave_around_unit("Base",self.faction,self.newship,"default",1+self.difficulty,3000.0,4000.0,self.significant)
 		if (self.enemy):
-		  if (runaway):
-		    self.enemy.setTarget(significant)
+		  if (self.runaway):
+		    self.enemy.SetTarget(significant)
 		    self.enemy.Jump()
 		    self.arrived=2
 		  else:
@@ -104,12 +101,12 @@ class bounty (Director.Mission):
 	  else:
 	    if (self.adjsys.Execute()):
 	      self.arrived=1
-	      self.newship=faction_ships.getRandomFighter(faction)
+	      self.newship=faction_ships.getRandomFighter(self.faction)
 	      localdestination=self.significant.getName()
 	      self.adjsys=go_somewhere_significant(self.you,0,500)
 	      VS.IOmessage (0,"bounty mission",self.mplay,"You must destroy the %s unit in this system." % (self.newship))
 	      self.obj=VS.addObjective("Destroy the %s unit" % (self.newship))
-	      if (runaway):	#ADD OTHER JUMPING IF STATEMENT CODE HERE
+	      if (self.runaway):	#ADD OTHER JUMPING IF STATEMENT CODE HERE
 	        VS.IOmessage (3,"bounty mission",self.mplay,"He is running towards the jump point.  Catch him!")
 	        VS.IOmessage (4,"bounty mission",self.mplay,"he is going to %s" % (localdestination))
 	      else:
