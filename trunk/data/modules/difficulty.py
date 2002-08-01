6a94f23f9fd99d62a6b346d1cde87f9e1c3da4db
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-08-01 09:03:01 +0000

    fixed a LOT of bugs in this module

--- trunk/data/modules/difficulty.py
+++ trunk/data/modules/difficulty.py
@@ -10,16 +10,13 @@ def SetDiff(diff):
 
 _key="31337ness"
 class difficulty:
-  diff=[]
-  creds=[]
-  credsToMax=1
-  i=0 #comment this if you want to check all in 1 frame
-  
   def SetDiff(self,diff):
     if (diff>VS.GetDifficulty()):
       VS.SetDifficulty(diff)
 
   def __init__(self,credsMax):
+    self.diff=[]
+    self.creds=[]
     self.credsToMax=credsMax
     un=VS.getPlayerX(0)
     self.i=0
@@ -27,11 +24,10 @@ class difficulty:
       newdiff=0
       if (Director.getSaveDataLength(self.i,_key)):
         newdiff=Director.getSaveData(self.i,_key,0)
-        self.diff+=[newdiff]
       else:
         newdiff=VS.GetDifficulty()
-        self.diff+=[newdiff]
         Director.pushSaveData(self.i,_key,newdiff)
+      self.diff+=[newdiff]        
       SetDiff(newdiff)
       self.creds+=[un.getCredits()]
       self.i+=1
@@ -44,21 +40,19 @@ class difficulty:
     return self.diff[playa]
   
   def Execute(self):
-#    for i in range(len(self.creds)): #uncomment this if you want to check all in 1 frame
-      if (len(self.creds)<=0):
-        if (self.i!=-1):
-          self.i=-1
-          raise IndexError("Empty creds and diff arrays in difficulty module\nUnable to find any players...")
-        return
-      if (self.i>=len(self.creds)):
-        self.i=0
+    if (self.i>=len(self.creds)):
+      self.i=0
+    else:
       un=VS.getPlayerX(self.i)
-      newcreds=un.getCredits()
-      if (self.creds[self.i]!=newcreds):
-        if (self.creds[self.i]>newcreds):
-          newdiff=((newcreds-self.creds[self.i])/self.credsToMax)
-          Director.putSaveData(self.i,_key,0,newdiff)
-          SetDiff(newdiff)
-        self.creds[self.i]=newcreds
-      self.i+=1 #comment this also if you want to check all in 1 frame
+      if (un):
+        newcreds=un.getCredits()
+        if (self.creds[self.i]!=newcreds):
+          if (self.creds[self.i]<newcreds):
+            newdiff=self.getPlayerDifficulty(i)+((newcreds-self.creds[self.i])/self.credsToMax)
+            if (newdiff>.999):
+              newdiff=.999
+            Director.putSaveData(self.i,_key,0,newdiff)
+            SetDiff(newdiff)
+          self.creds[self.i]=newcreds
+      self.i+=1
   
