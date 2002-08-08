afbb970e299cf6ad5d4fd04bc255a65ac2b52e91
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-08-08 03:59:02 +0000

    changed teh uni and industrial lib

--- bases/university_day.py
+++ bases/university_day.py
@@ -3,4 +3,4 @@ import sys
 import industrial_lib
 time_of_day='_day'
 
-(landing_platform,bar,weap) = industrial_lib.MakeCorisc (time_of_day)
+(landing_platform,bar,weap) = industrial_lib.MakeCorisc (time_of_day,'bases/bartender_union.py')
