44acff302f70a05568204aeec0983ab07b88f3e9
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-09-01 21:16:14 +0000

    added a hire wingman mission

---
+++ modules/wingman.py
@@ -0,0 +1,60 @@
+from go_to_adjacent_systems import *
+from go_somewhere_significant import *
+import random
+import launch
+import faction_ships
+import VS
+import Briefing
+import universe
+import unit
+import Director
+class wingman (Director.Mission):
+    def __init__ (self,price,factionname,numships):
+        Director.Mission.__init__(self)
+        self.price=price
+        self.faction=factionname
+        self.you = VS.getPlayer()
+        self.numships=numships
+        self.adjsys=go_somewhere_significant (self.you,0,5000,0)
+        self.wingship = faction_ships.getRandomFighter(factionname)
+        nam = "[%s]" % self.wingship
+        self.adjsys.Print(nam+" Hello I'm waiting for your arrival at %s" ,"game")
+        VS.IOmessage(1,"game","all",nam+" Once you meet me")
+        VS.IOmessage(2,"game","all",nam+" I will obey your commands until our contracts expire.")
+
+    def GenerateWingmen(self):
+        if (self.you):
+            if (self.you.getCredits()>=self.price):
+                self.you.addCredits(-self.price)
+                lead=launch.launch_wave_around_unit(self.you.getFlightgroupName(),
+                                                    self.you.getFactionName(),
+                                                    self.wingship,
+                                                    "default",
+                                                    self.numships,
+                                                    500,
+                                                    1000,
+                                                    self.you)
+                VS.IOmessage (0,"wingman","all","[%s] I am at your service commander. Let us succeed!"%lead.getName())
+            else:
+                lead=launch.launch_wave_around_unit("ForHire",
+                                                    self.faction,
+                                                    self.wingship,
+                                                    "default",
+                                                    self.numships,
+                                                    500,
+                                                    1000,
+                                                    self.you)
+                lead.SetTarget(self.you)
+                VS.IOmessage (0,"game","all","[%s] You don't have the agreed money... prepare to DIE")
+    def Execute (self):
+        
+        if (self.adjsys.Execute()):
+            self.GenerateWingmen()
+            VS.terminateMission(1)
+    def initbriefing(self):
+        print "ending briefing"                
+    def loopbriefing(self):
+        print "loop briefing"
+        Briefing.terminate();
+    def endbriefing(self):
+        print "ending briefing"        
