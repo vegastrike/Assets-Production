cc881c713ab44643613d3a417b423f74737952e4
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-09-07 22:26:05 +0000

    added the new moduels

--- trunk/data/modules/wc1_mis0.py
+++ trunk/data/modules/wc1_mis0.py
@@ -2,24 +2,23 @@ import random
 import VS
 import unit
 import launch
+from wc1 import findOriginAndMove
 from Vector import Add
 class wc1_mis0:
     def __init__(self):
         pass
     def Start(self,carrier):
-        self.origin =unit.getJumpPoint(random.randrange(0,99)).Position()
-        print self.origin
+        self.inflight=0
+        self.origin = findOriginAndMove(carrier).Position();
         self.carrier=carrier
-        self.origin=Add(self.origin,(1000+carrier.rSize(),100,100))
-        carrier.SetPosAndCumPos (self.origin)
-        self.spirit= launch.launch (VS.getPlayer().getFlightgroupName(),"confed","nova","default",1,1,Add((1000,200,0),self.origin))
+        self.wingman= launch.launch (VS.getPlayer().getFlightgroupName(),"confed","nova","default",1,1,Add((1000,200,0),self.origin))
         self.nav=[]
         self.visited=[0,0,0]
         self.launched=[0,0,0]
         self.nav+=[launch.launch("nav1","neutral","navpoint","sitting_duck",1,1,Add(self.origin,(50000,40000,10000)))]
         self.nav+=[launch.launch("nav2","neutral","navpoint","sitting_duck",1,1,Add(self.origin,(10000,100000,10000)))]
-        VS.launch("Asteroids","neutral","asteroid","AFieldSparse","default",1,1,Add(self.origin,(10000,100000,10000)),"")
-        VS.launch("Asteroids","neutral","asteroid","AFieldThin","default",1,1,Add(self.origin,(50000,40000,10000)),"")
+        self.roids = [VS.launch("Asteroids","neutral","AFieldSparse","asteroid","default",1,1,Add(self.origin,(10000,100000,10000)),""),
+                      VS.launch("Asteroids","neutral","AFieldThin","asteroid","default",1,1,Add(self.origin,(50000,40000,10000)),"")]
         self.nav+=[launch.launch("nav3","neutral","navpoint","sitting_duck",1,1,Add(self.origin,(-40000,40000,10000)))]
     def LaunchNav (self,i,playa):
         if (i==0):
@@ -27,23 +26,32 @@ class wc1_mis0:
         elif (i==2):
             launch.launch_wave_around_unit("BadGuys","aera","kyta","default",2,100,1000,playa)
     def EndMission (self):
+        self.wingman.Kill()
+        for n in self.nav:
+            n.Kill()
+        for n in self.roids:
+            n.Kill()
         if (self.visited[0] and self.visited[1] and self.visited[2]):
             #success (change debrief maybe?)
-            return ("vega_sector/enyo",1)
+            return ("vega_sector/vega",0)
         else:
             #failed
-            return ("vega_sector/enyo",1) 
+            return ("vega_sector/vega",0) 
     def Execute(self):
         playa= VS.getPlayer()
-        for i in range (len(self.nav)):
-            if (not self.launched[i]):
-                if (playa.getDistance (self.nav[i])<1000):
-                    self.LaunchNav(i,playa)
-                    self.launched[i]=1
+        if (playa):
+            for i in range (len(self.nav)):
+                if (self.nav[i]):
+                    if (not self.launched[i]):                    
+                        if (playa.getDistance (self.nav[i])<1000):
+                            self.LaunchNav(i,playa)
+                            self.launched[i]=1
+                    else:
+                        if (playa.getDistance (self.nav[i])<100):
+                            self.visited[i]=1
+            if (playa.isDocked(self.carrier) or self.carrier.isDocked(playa)):
+                if (self.inflight):
+                    return self.EndMission()
             else:
-                if (playa.getDistance (self.nav[i])<100):
-                    self.visited[i]=1
-            
-        if (playa.isDocked(self.carrier) or self.carrier.isDocked(playa)):
-            return self.EndMission()
+                self.inflight=1
         return
