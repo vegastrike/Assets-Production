bfd47c0194902fc87569a36cf3c0bfec206f5575
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-11-25 11:03:07 +0000

    fixed total war to be cooler

--- trunk/data/modules/total_war.py
+++ trunk/data/modules/total_war.py
@@ -11,11 +11,9 @@ class total_war (Director.Mission):
         print "init1INITNITNITNTINTINTINTINTITNINTINTINTINTINT\nININTINTINTITNITNITNTINTINTINTINTITNITNITNITNITNITNITNITNITNIT"
         VS.SetDifficulty(.9)
         Director.Mission.__init__(self)
-        self.lasttime=0.0
+        self.lasttime=-1000
         self.waittime=10.0
-        print "init"
     def launch_new_wave(self):
-        print "testing"
         side = vsrandom.randrange(0,2)
         faction="confed"
         ai = vsrandom.randrange(0,2)
@@ -27,10 +25,10 @@ class total_war (Director.Mission):
             faction=faction_ships.get_enemy_of("confed")
         else:
             faction=faction_ships.get_friend_of("confed")
-        launched = launch.launch_wave_around_unit ("Shadow",faction,faction_ships.getRandomFighter(faction),ai,vsrandom.randrange(1,10),2000.0,4000.0,VS.getPlayer(),'')
-        
+        launched = launch.launch_wave_around_unit ("Shadow",faction,faction_ships.getRandomFighter(faction),ai,vsrandom.randrange(1,10),100.0,2000.0,VS.getPlayer(),'')
+        if (vsrandom.randrange(0,10)==0):
+            launch.launch_wave_around_unit ("ShadowCap",faction,faction_ships.getRandomCapitol(faction),ai,1,2000.0,4000.0,VS.getPlayer(),'')
     def Execute (self):
-        print "ex"
         time = VS.GetGameTime()
         if (time-self.lasttime>self.waittime):
             self.launch_new_wave()
