d6f63db044e3a6515e6df6271134b6f74cbdcd22
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-12-27 22:52:45 +0000

    fixed the ISO missions to not fail if you quit and reload

--- bases/fixers/iso_mission.py
+++ bases/fixers/iso_mission.py
@@ -2,21 +2,21 @@ import fixers
 import Base
 fixers.DestroyActiveButtons ()
 playernum = VS.getPlayer().isPlayerStarship()
-if (not Director.getSaveDataLength(playernum,"iso_mission1")):
+if (fixers.checkSaveValue(playernum,"iso_mission1",0) and VS.numActiveMissions()<=1):
 	fixers.setSaveValue (playernum,"iso_mission1",0)
 	#load mission 1
 	VS.LoadMission ("mission/defend/iso/defend_iso_mission1.mission")
-elif ((not Director.getSaveDataLength (playernum,"iso_mission2")) and fixers.checkSaveValue (playernum,"iso_mission1",1)):
+elif (fixers.checkSaveValue(playernum,"iso_mission2",0) and VS.numActiveMissions()<=1 and fixers.checkSaveValue (playernum,"iso_mission1",1)):
 	fixers.setSaveValue (playernum,"iso_mission2",0)
 	VS.LoadMission ("mission/escort/iso/escort_iso_mission2.mission")
 	#load mission 2
-elif ((not Director.getSaveDataLength (playernum,"iso_mission3")) and fixers.checkSaveValue (playernum,"iso_mission2",1)):
+elif (fixers.checkSaveValue(playernum,"iso_mission3",0) and VS.numActiveMissions()<=1 and fixers.checkSaveValue (playernum,"iso_mission2",1)):
 	fixers.setSaveValue (playernum,"iso_mission3",0)
 	VS.LoadMission ("mission/cargo/iso/cargo_contraband_mission3.mission")
 	#load mission 3
-elif ((not Director.getSaveDataLength (playernum,"iso_mission4")) and fixers.checkSaveValue (playernum,"iso_mission3",1)):
+elif (fixers.checkSaveValue(playernum,"iso_mission4",0) and VS.numActiveMissions()<=1 and fixers.checkSaveValue (playernum,"iso_mission3",1)):
 	fixers.setSaveValue (playernum,"iso_mission4",0)
 	VS.LoadMission ("mission/defend/defend_iso_mission4.mission")
 	#load mission 4
 else:
-	Base.message ("Excuse me... I have an urgent holo-call from the party...")
+	Base.message ("Our records indicate that you are already completing a mission. Come back to me when you're done with that business first.")
