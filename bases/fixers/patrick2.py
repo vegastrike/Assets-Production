5e2115175ed6f519b82efef3412ff7039f199784
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-12-27 04:09:24 +0000

    added fixer missions

--- bases/fixers/patrick2.py
+++
@@ -1,7 +0,0 @@
-import Base
-import fixers
-
-
-fixers.DestroyActiveButtons ()
-Base.Message("""We've checked your account, and you don't have enough credits to buy this ship.
-Come back to me when you have more credits, and don't be embarrassed.  These things happen.""")
