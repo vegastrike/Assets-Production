ea36d1d9ecf08f8a497c4c1f016738d07bbf0132
jacks <jacks@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-01-03 04:48:08 +0000

    edited for content

--- bases/fixers/pirates.py
+++ bases/fixers/pirates.py
@@ -18,7 +18,7 @@ else:
 		Base.Message("We are in need of a pilot who can safely transport this cargo to one of our bases in the #55ffffrigel#000000 system. It is highly important that no confed or militia know of these goods. Will you accept this unavoidable offer?")
 #And then if you fail.......
 	elif (fixers.checkSaveValue (playernum,"pirate_mission1",-1) or fixers.checkSaveValue (playernum,"pirate_mission2",-1) or fixers.checkSaveValue (playernum,"pirate_mission3",-1) or fixers.checkSaveValue (playernum,"pirate_mission4",-1)):
-		Base.Message ("How could you let our precious cargo be destroyed. It has cost us millions of credits and you trashed it like it was nothing. Prepare to be naled,pal!")
+		Base.Message ("How could you let our precious cargo be destroyed. It has cost us millions of credits and you trashed it like it was nothing. Prepare to be nailed,pal!")
 		type = faction_ships.getRandomFighter ("pirates")
 		fgname="shadow"
 		launch.launch_wave_around_unit (fgname,"pirates",type,"default",1,80,300,playa).SetTarget(playa)
@@ -29,7 +29,7 @@ else:
 			fixers.payCheck(playernum,'paidpiratemission',0,20000)
 			AssignMission()
 		elif (fixers.checkSaveValue (playernum,"pirate_mission2",1) and fixers.checkSaveValue(playernum,"pirate_mission3",0)):			
-			Base.Message ("Hey, pal. You got back here all right. Now I have a special mission for you that requires a tractor beam. This will require you to destroy a merchant transport in this system. You must have a tractor beam equipped to your ship.")#assign mis 3:
+			Base.Message ("Hey, pal. You got back here all right. Now I have a special mission for you that requires a tractor beam. This will require you to destroy a merchant transport in this system. You must have a tractor beam equipped to your ship...we want the ship to blow, but the cargo...")#assign mis 3:
 			fixers.payCheck(playernum,'paidpiratemission',1,20000)
 			AssignMission()
 		elif (fixers.checkSaveValue (playernum,"pirate_mission3",1) and fixers.checkSaveValue(playernum,"pirate_mission4",0)):
@@ -41,8 +41,8 @@ else:
 			AssignMission()
 		elif (fixers.checkSaveValue(playernum,"pirate_mission4",1)):
 			fixers.setSaveValue(playernum,"pirate_mission4",2)
-			Base.Message ("Thanks, pal.  You have become worthy to be a friend of the pirates.You have saved many of our lives here in the rigel system. Here are your 80000 credits that we promised.")
-			fixers.payCheck(playernum,'paidpiratemission',3,35000)
+			Base.Message ("Thanks, pal. You've earned our trust. You're a recognized friend of the pirates now. I don't know if you fully grok this yet, but you have saved many of our lives here in the Rigel system. Here are your 80000 credits that we promised. Go out, take what's yours, and don't let anyone give you flak.")
+			fixers.payCheck(playernum,'paidpiratemission',3,80000)
 		else:
 			pass
-##			Base.Message ("Make haste for the people! Tardiness is the opium for the masses!") 
+
