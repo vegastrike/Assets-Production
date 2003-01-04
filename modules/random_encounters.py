b4d4fb1f9d62cd8a7b196fff9231e191136207c9
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-01-04 10:29:53 +0000

    fewer capships within the borders

--- modules/random_encounters.py
+++ modules/random_encounters.py
@@ -116,6 +116,8 @@ class random_encounters:
         if (i==0 and vsrandom.random()<(.3+(.4*VS.GetDifficulty()))):
           localfaction=near_faction
           gen_num_ships*=3
+      else:
+	self.cap_prob/=3;#1/3 chance of capships within a nation border
       numship= vsrandom.randrange(1,int(gen_num_ships)+1)
       self.det_distance = self.cur.detection_distance
       launch_recycle.launch_wave_around(localfaction,localfaction,"default",numship,0,self.generation_distance*vsrandom.random()*0.9,un, 2.0*self.det_distance,"")
