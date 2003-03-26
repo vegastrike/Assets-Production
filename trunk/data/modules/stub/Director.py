3f9274c9e593b42cfbfd4839ee7d05e3fef763b4
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-03-26 23:19:54 +0000

    prints out when savedata is getting changed in director fixed some import bugs in VS.py

--- trunk/data/modules/stub/Director.py
+++ trunk/data/modules/stub/Director.py
@@ -12,15 +12,20 @@ class Mission:
 def putSaveData (whichplayer, key, num, val):
     global _savedata
     _savedata[whichplayer][key][num]=val
+    print 'DIRECTOR:putSaveData: %g --> Player%d["%s"][%d]'%(val,whichplayer,key,num)
     return len(_savedata[whichplayer][key])
 def getSaveDataLength (whichplayer, key):
     global _savedata
     try:
-	return len(_savedata[whichplayer][key])
+        lent=len(_savedata[whichplayer][key])
+        print 'DIRECTOR:getSaveDataLength len(Player%d["%s"])=%d'%(whichplayer,key,lent)
+	return lent
     except:
+        print 'DIRECTOR:getSaveDataLength len(Player%d["%s"])=EMPTY'%(whichplayer,key)
 	return 0
 def getSaveData (whichplayer, key,num):
     global _savedata
+    print 'DIRECTOR:getSaveData Player%d["%s"][%d]=%g'%(whichplayer,key,num,_savedata[whichplayer][key][num])
     return _savedata[whichplayer][key][num]
 def pushSaveData (whichplayer,key,val):
     global _savedata
@@ -28,23 +33,30 @@ def pushSaveData (whichplayer,key,val):
 	_savedata[whichplayer][key].append(val)
     except:
 	_savedata[whichplayer][key]=[val]
+    print 'DIRECTOR:pushSaveData: %g --> Player%d["%s"][%d]'%(val,whichplayer,key,len(_savedata[whichplayer][key]))
     return len(_savedata[whichplayer][key])
 def eraseSaveData (whichplayer,key,num):
     global _savedata
     del _savedata[whichplayer][key][num]
+    print 'DIRECTOR:eraseSaveData: del Player%d["%s"][%d]'%(whichplayer,key,num)
     return len(_savedata[whichplayer][key])
 def putSaveString (whichplayer, key, num, val):
     global _savestrs
+    print 'DIRECTOR:putSaveString: "%s" --> StrPlayer%d["%s"][%d]'%(val,whichplayer,key,num)
     _savestrs[whichplayer][key][num]=val
     return len(_savestrs[whichplayer][key])
 def getSaveStringLength (whichplayer, key):
     global _savestrs
     try:
-	return len(_savestrs[whichplayer][key])
+        lent=len(_savestrs[whichplayer][key])
+        print 'DIRECTOR:getSaveStringLength len(StrPlayer%d["%s"])=%d'%(whichplayer,key,lent)
+        return lent
     except:
+        print 'DIRECTOR:getSaveStringLength len(StrPlayer%d["%s"])=EMPTY'%(whichplayer,key)
 	return 0
 def getSaveString (whichplayer, key,num):
     global _savestrs
+    print 'DIRECTOR:getSaveString StrPlayer%d["%s"][%d]="%s"'%(whichplayer,key,num,_savestrs[whichplayer][key][num])
     return _savestrs[whichplayer][key][num]
 def pushSaveString (whichplayer,key,val):
     global _savestrs
@@ -52,9 +64,11 @@ def pushSaveString (whichplayer,key,val):
 	_savestrs[whichplayer][key].append(val)
     except:
 	_savestrs[whichplayer][key]=[val]
+    print 'DIRECTOR:pushSaveString: "%s" --> StrPlayer%d["%s"][%d]'%(val,whichplayer,key,len(_savestrs[whichplayer][key]))
     return len(_savestrs[whichplayer][key])
 def eraseSaveString (whichplayer,key,num):
     global _savestrs
     del _savestrs[whichplayer][key][num]
+    print 'DIRECTOR:eraseSaveString: del StrPlayer%d["%s"][%d]'%(whichplayer,key,num)
     return len(_savestrs[whichplayer][key])
 
