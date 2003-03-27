6bd72b60c1146e0bfbd943d2be428f3c33b1ccc8
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-03-27 00:23:58 +0000

    seems to work now! does get some ""s sometimes...

--- trunk/data/modules/dynamic_universe.py
+++ trunk/data/modules/dynamic_universe.py
@@ -16,8 +16,8 @@ def GenerateFgShips (maxshipinfg,factionnr):
 	lst=[]
 	capship=()
 	if vsrandom.random()<.1:
-		capship=(faction_ships.getRandomCapitolInt(factionnr),1)
-	return (faction_ships.getRandomFighterInt(factionnr),maxshipinfg)+capship
+		capship=((faction_ships.getRandomCapitolInt(factionnr),1),)
+	return ((faction_ships.getRandomFighterInt(factionnr),maxshipinfg),)+capship
 
 def GenerateAllShips (numflightgroups,maxshipinfg):
 	for fnr in range(faction_ships.getMaxFactions()-1):
@@ -28,15 +28,17 @@ def GenerateAllShips (numflightgroups,maxshipinfg):
 
 def AddSysDict (cursys):
 	#pick random fighter from insysenemies with .3 probability OR pick one from the friendlies list.
+	print 'Addsysdict'
 	sysfaction=VS.GetGalaxyProperty(cursys,"faction")
 	global fgnames, fglists
+	i=0
 	for i in range (vsrandom.randrange(10)): #number of fgs in a system.
-		facion=sysfaction
-		if vsrandom.random()<.3:
+		faction=sysfaction
+		if vsrandom.random()<.3 or sysfaction=='unknown':
 			faction=faction_ships.get_insys_enemy_of(sysfaction)
 		else:
 			faction=faction_ships.get_friend_of(sysfaction)
-		factionnr=faction_ships.FactionToInt(factionnr)
+		factionnr=faction_ships.factionToInt(faction)
 		typenumbertuple=fglists[factionnr].pop(-1) #pop returns item inside array
 		fgname=fgnames[factionnr].pop(-1) #pop returns item inside array
 		if not len(fgnames):
@@ -50,12 +52,12 @@ def Makesys (startingsys):
 	global origfgnames
 	origfgnames=fgnames
 	origfglists=fglists
-	systemdict[startingsys]=AddSysDict(cursys)
+	systemdict[startingsys]=AddSysDict(startingsys)
 	todo=getAdjacentSystemList(startingsys)
 	while len(todo):
 		tmptodo=todo.pop(-1)
 		if (not systemdict.has_key(tmptodo)):
-			todo+=universe.getAdjacentSystemList(tmptodo)
+			todo+=getAdjacentSystemList(tmptodo)
 			systemdict[tmptodo]=AddSysDict(tmptodo)
 	return len(systemdict)
 
@@ -63,7 +65,7 @@ genUniverse=-1
 if cp>=0:
 	genUniverse=0
 	curfaclist = fg_util.AllFactions()
-	if (fg_util.ReadStringList(cp,"FactionRefList")==curfaclist):
+	if (fg_util.ReadStringList(cp,"FactionRefList")!=curfaclist):
 		fg_util.WriteStringList(cp,"FactionRefList",curfaclist)
 		print 'generating ships... ... ...'
 		GenerateAllShips (5000,5) ###Insert number of flight groups and max ships per fg
@@ -71,6 +73,7 @@ if cp>=0:
 		genUniverse=Makesys(VS.getSystemFile())
 		#now every system has distributed ships in the save data!
 	#TODO: add ships to current system (for both modes)  uru?
+	print 'Purging...'
 	for i in fg_util.AllFactions():
 		fg_util.PurgeZeroShips(i)
 
