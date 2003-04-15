0870accbaffa8be1681eb6134cf09e67f4ff9114
dandandaman <dandandaman@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-04-15 13:01:35 +0000

    updated

--- modules/ai_qct_waitjump.py
+++ modules/ai_qct_waitjump.py
@@ -18,32 +18,34 @@ class waitjump(VS.PythonAI):
 		self.AddReplaceLastOrder(1)
 		self.timer = 0
 		self.got_target = 0
-
+		self.autoed = 0
 
 	def Execute(self):
 		VS.PythonAI.Execute(self);
 		if quest_contraband_truck.truck_exit == 1:
 			if self.got_target == 0:
-				self.trucktarget = ((self.GetParent()).GetTarget())
 				self.GetParent().SetTarget(universe.getRandomJumppoint())
 				self.trucktarget = (self.GetParent()).GetTarget()
+			        self.XMLScript ("++afterburn-turntowards.xml")
+			        self.AddReplaceLastOrder(1)
 				self.got_target = 1
-			self.trucktarget_locat = self.trucktarget.Position()
+
 # starts him afterburning to target
-			self.FaceTarget(1)
-			self.AddReplaceLastOrder(1)
 
 			if self.timer == 0:
 				self.timer = VS.GetGameTime()
 				print "Timer Set"
-			elif self.timer + 5 < VS.GetGameTime():
-				self.MoveTo(self.trucktarget_locat,1)
-				self.AddReplaceLastOrder(1)
+			elif self.timer + 30 < VS.GetGameTime() and self.autoed == 0:
+				self.GetParent().AutoPilotTo(self.trucktarget,1)
 				self.GetParent().ActivateJumpDrive(1)
+				self.autoed = 1
+			elif self.timer + 5 < VS.GetGameTime():
+				self.MoveTo(self.trucktarget.Position(),1)
+				self.AddReplaceLastOrder(1)
 #			elif self.timer + 60 < VS.GetGameTime():
 # gets him to auto to the jump and jump out
 #				self.GetParent().ActivateJumpDrive(1)
-			return 1
+
 
 		return 1
 hi1 = waitjump()
