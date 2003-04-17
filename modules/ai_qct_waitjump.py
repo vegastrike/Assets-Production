b9737b7f8c87d3d3445abce45fa657d4d25df334
dandandaman <dandandaman@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-04-17 07:16:26 +0000

    updated

--- modules/ai_qct_waitjump.py
+++ modules/ai_qct_waitjump.py
@@ -20,6 +20,9 @@ class waitjump(VS.PythonAI):
 		self.got_target = 0
 		self.autoed = 0
 
+	def ChooseTarget(self):
+		return
+
 	def Execute(self):
 		VS.PythonAI.Execute(self);
 		if quest_contraband_truck.truck_exit == 1:
@@ -36,8 +39,8 @@ class waitjump(VS.PythonAI):
 				self.timer = VS.GetGameTime()
 				print "Timer Set"
 			elif self.timer + 30 < VS.GetGameTime() and self.autoed == 0:
-				self.GetParent().AutoPilotTo(self.trucktarget,1)
 				self.GetParent().ActivateJumpDrive(1)
+				self.GetParent().AutoPilotTo(self.trucktarget,1)
 				self.autoed = 1
 			elif self.timer + 5 < VS.GetGameTime():
 				self.MoveTo(self.trucktarget.Position(),1)
@@ -45,7 +48,7 @@ class waitjump(VS.PythonAI):
 #			elif self.timer + 60 < VS.GetGameTime():
 # gets him to auto to the jump and jump out
 #				self.GetParent().ActivateJumpDrive(1)
-
+			print self.GetParent().getMinDis(self.trucktarget.Position())
 
 		return 1
 hi1 = waitjump()
