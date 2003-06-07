28e57b20c631faf9a11c800c44c97d90656849cf
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-06-07 23:19:27 +0000

    fixed some dyn. mission stuff

--- trunk/data/modules/stub/stubloader.py
+++ trunk/data/modules/stub/stubloader.py
@@ -1,9 +1,12 @@
 import sys
-sys.path += ['../modules/stub/','../modules','../bases']
+import VS
+import Director
+sys.path += ['../../modules/stub/','../../modules','../../bases']
 def kfac (k):
 	if (k==0):
 		return 1
 	return k*kfac(k-1)
+Director.dontdoprint=True
 import generate_dyn_universe
 import dynamic_battle
 dynamic_battle.UpdateCombatTurn()
@@ -13,4 +16,12 @@ aera = fg_util.SortedAllShips ('aera')
 def doit(n=1000):
 	for i in range(n):
 		dynamic_battle.UpdateCombatTurn()
+
 	
+doit(100000)
+print 'hi'
+import dynamic_mission
+Director.dontdoprint=False
+#VS.pushSystem("SYSTEMvs")
+dynamic_mission.CreateMissions()
+#VS.popSystem()
