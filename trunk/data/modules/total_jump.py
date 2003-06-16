bff6f8e54c0506319ea21a5a742cec304bd9cfdc
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-06-16 22:22:25 +0000

    Fixed total_jump.mission to also autopilot

--- trunk/data/modules/total_jump.py
+++ trunk/data/modules/total_jump.py
@@ -6,16 +6,14 @@ import Briefing
 import universe
 import unit
 import Director
-class total_jump (Director.Mission):
+class total_jump:
     def __init__(self):
         VS.SetDifficulty(.1)
-        Director.Mission.__init__(self)
         self.lasttime=-1000
         self.waittime=5.0
     def launch_new_wave(self):
+	un = VS.getPlayer()
 	if (vsrandom.randrange(0,4)==0):
-		print "blehrandrange"
-		un = VS.getPlayer()
 		if (un):
 			currentsystem = VS.getSystemFile()
 			numadj=VS.GetNumAdjacentSystems(currentsystem)
@@ -23,23 +21,38 @@ class total_jump (Director.Mission):
 				cursys=VS.GetAdjacentSystem(currentsystem,vsrandom.randrange(0,numadj))
 			else:
 				cursys = 'enigma_sector/heavens_gate'
-			print "jumping to "+cursys
+			print "TJ: jumping to "+cursys
 			un.JumpTo(cursys)
+		else:
+			print "TJ: jumping to [ERROR: you are null]"
 		return
-        side = vsrandom.randrange(0,2)
-        faction="confed"
-        ai = vsrandom.randrange(0,2)
-        if (ai==0):
-            ai = "printhello.py"
-        else:
-            ai = "default"
-        if (side==0):
-            faction=faction_ships.get_enemy_of("confed")
-        else:
-            faction=faction_ships.get_friend_of("confed")
-        launched = launch.launch_wave_around_unit ("Shadow",faction,faction_ships.getRandomFighter(faction),ai,vsrandom.randrange(1,10),100.0,2000.0,VS.getPlayer(),'')
-        if (vsrandom.randrange(0,10)==0):
-            launch.launch_wave_around_unit ("ShadowCap",faction,faction_ships.getRandomCapitol(faction),ai,1,2000.0,4000.0,VS.getPlayer(),'')
+	else:
+		siglist=universe.significantUnits()
+		if len(siglist)==0:
+		    print "TJ: siglist empty"
+		    return
+		sig=siglist[vsrandom.randrange(0,len(siglist))]
+		if (not sig):
+		    print "TJ: sig null"
+		    return
+		print "TJ: autopiloting to "+sig.getName()
+		un.AutoPilotTo(sig,True)
+		un.SetTarget(sig)
+                
+##        side = vsrandom.randrange(0,2)
+##        faction="confed"
+##        ai = vsrandom.randrange(0,2)
+##        if (ai==0):
+##            ai = "printhello.py"
+##        else:
+##            ai = "default"
+##        if (side==0):
+##            faction=faction_ships.get_enemy_of("confed")
+##        else:
+##            faction=faction_ships.get_friend_of("confed")
+##        launched = launch.launch_wave_around_unit ("Shadow",faction,faction_ships.getRandomFighter(faction),ai,vsrandom.randrange(1,10),100.0,2000.0,VS.getPlayer(),'')
+##        if (vsrandom.randrange(0,10)==0):
+##            launch.launch_wave_around_unit ("ShadowCap",faction,faction_ships.getRandomCapitol(faction),ai,1,2000.0,4000.0,VS.getPlayer(),'')
     def Execute (self):
 #        un=VS.getUnit(0);
 #        i=0
