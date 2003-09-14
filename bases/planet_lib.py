bc636f089212cc7d31ff7393ddfb2bfcd7ea3adc
mikebyron <mikebyron@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-09-14 04:51:55 +0000

    Change Python mode strings to reflect new gui.

--- bases/planet_lib.py
+++ bases/planet_lib.py
@@ -8,7 +8,7 @@ def MakePlanet (time_of_day='_day'):
     room1 = Base.Room ('Scenery')    
     Base.Texture (room1, 'tex', 'bases/generic/generic'+time_of_day+'.spr', 0, 0)
     Base.Texture (room1, 'tex', 'bases/generic/holo.spr', -.5, -.4)
-    Base.Comp (room1, 'Computer', -0.6, -0.8, .2578906, 0.80833, 'Computer', 'BUYMODE SELLMODE UPGRADEMODE DOWNGRADEMODE INFOMODE SHIPMODE NEWSMODE MISSIONMODE BRIEFINGMODE')
+    Base.Comp (room1, 'Computer', -0.6, -0.8, .2578906, 0.80833, 'Computer', 'Cargo Upgrade Info ShipDealer News Missions')
     Base.Ship (room1, 'ship', (0,-.5,4), (0,.93,-.34), (-1,0,0))
     Base.LaunchPython (room1, 'launch','bases/launch_music.py', -1, -1, 2, .8, 'Launch Your Ship')
 
