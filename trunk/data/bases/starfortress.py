87c20f0dabc78545fca1d7587e5098c3709ddc58
pyramid3d <pyramid3d@9476613a-4e0a-0410-bcee-947800e9c4d1>
2008-10-27 20:11:32 +0000

    Ship updates by Fendorin. Mk32 high poly mesh, turrets, thruster, blink lights. H496 silver (default) and gold editions including turrets, thruster, blink lights.

--- trunk/data/bases/starfortress.py
+++
@@ -1,5 +0,0 @@
-import VS
-plist=VS.musicAddList('starfortress.m3u')
-VS.musicPlayList(plist)    
-import military_lib
-(room1, room2, bar, weap) = military_lib.MakeMilitaryBase('bases/bartender_military.py')
