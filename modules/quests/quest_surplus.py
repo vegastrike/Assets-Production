fe8629673f531ad2230265c1b1f5116ff4f820c4
klaussfreire <klaussfreire@9476613a-4e0a-0410-bcee-947800e9c4d1>
2010-09-09 14:54:58 +0000

    Bugfix: bug in quest_surplus found by nikai

--- modules/quests/quest_surplus.py
+++ modules/quests/quest_surplus.py
@@ -37,10 +37,11 @@ def makeSurplusShortage():
     (sys,mylist)=universe.getAdjacentSystems(VS.getSystemFile(),vsrandom.randrange(1,3))
     adj=vsrandom.uniform (-.5,.5)
     myquest=0
-    cats = (VS.getRandCargo(1,"").GetCategory(), VS.getRandCargo(1,"").GetCategory())
+    cats = tuple(set([VS.getRandCargo(1,"").GetCategory(), VS.getRandCargo(1,"").GetCategory()]))
     news=""
     if (adj<0):
-        news = "SURPLUS IN %s:  There are reports of surpluses of %s and %s items in the %s system.  Wise Privateers have been purchasing the surpluses in hopes of future gains elsewhere." % (sys,cats[0],cats[1],sys)
+        news = "SURPLUS IN %s:  There are reports of surpluses of %s items in the %s system.  Wise Privateers have been purchasing the surpluses in hopes of future gains elsewhere." % (sys," and ".join(cats),sys)
     else:
-        news = "DEMAND FOR %s AND %s in %s:  Citizens in the %s system have been frusterated by the expensive prices and low quantities of %s and %s goods.  Privateers are currently banding together to try to bring supply to meet the demand.  But while prices are still high, some big traders could certainly capitalize on this shortage." % (cats[0],cats[1],sys,sys,cats[0],cats[1])
+        news = "DEMAND FOR %s in %s:  Citizens in the %s system have been frusterated by the expensive prices and low quantities of %s goods.  Privateers are currently banding together to try to bring supply to meet the demand.  But while prices are still high, some big traders could certainly capitalize on this shortage." % (" AND ".join(cats),sys,sys," and ".join(cats))
     return (sys,quest_surplus_factory(cats,1+adj,1-adj,1,1),news)
+
