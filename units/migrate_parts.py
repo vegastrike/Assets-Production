63277d837f1133a850209465768655d55c7db09e
Roy Falk <roy@falk.co.il>
2022-07-14 18:17:33 +0300

    Minor changes to python script to align to best practices.

--- units/migrate_parts.py
+++ units/migrate_parts.py
@@ -1,8 +1,28 @@
 import json
+import sys
 
-keys = ('file','categoryname','price','mass','volume',	'description')
+master_array = []
+
+def main():
+    with open("../master_part_list_2.csv", "r") as parts_file:
+        lines = parts_file.readlines()
+        first_line = True
+        for line in lines:
+            if first_line:
+                first_line = False
+                continue
+            line = line.rstrip('\n')
+            parseLine(line)
+
+    json_object = json.dumps(master_array, indent = 4)
+    print(json_object)
+    with open("../master_part_list.json", "w") as json_file:
+        json_file.write(json_object)
+
 
 def parseLine(line):
+    keys = ('file','categoryname','price','mass','volume',	'description')
+    
     values = line.split(',')
     dictionary = {}
     i = 0
@@ -17,19 +37,7 @@ def parseLine(line):
         i = i + 1
 
     master_array.append(dictionary)
-
-master_array = []
-
-with open("../master_part_list_2.csv", "r") as parts_file:
-    lines = parts_file.readlines()
-    first_line = True
-    for line in lines:
-        if first_line:
-            first_line = False
-            continue
-        line = line.rstrip('\n')
-        parseLine(line)
-
-json_object = json.dumps(master_array, indent = 4)
-with open("../master_part_list.json", "w") as json_file:
-    json_file.write(json_object)
+    
+# by default execute `main()` when loaded as a script
+if __name__ == "__main__":
+   sys.exit(main())
