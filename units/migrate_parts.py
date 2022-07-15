b0d4e73c0287ab8d510f827e382281df828d6090
Roy Falk <roy@falk.co.il>
2022-07-15 22:35:59 +0300

    Two minor edits to conversion script.

--- units/migrate_parts.py
+++ units/migrate_parts.py
@@ -1,9 +1,11 @@
 import json
 import sys
 
-master_array = []
+
 
 def main():
+    master_array = []
+    
     with open("../master_part_list_2.csv", "r") as parts_file:
         lines = parts_file.readlines()
         first_line = True
@@ -12,12 +14,10 @@ def main():
                 first_line = False
                 continue
             line = line.rstrip('\n')
-            parseLine(line)
+            master_array.append(parseLine(line))
 
-    json_object = json.dumps(master_array, indent = 4)
-    print(json_object)
     with open("../master_part_list.json", "w") as json_file:
-        json_file.write(json_object)
+        json.dump(master_array, json_file, indent=4)
 
 
 def parseLine(line):
@@ -36,7 +36,8 @@ def parseLine(line):
         dictionary[keys[i]] = value
         i = i + 1
 
-    master_array.append(dictionary)
+    return dictionary
+    
     
 # by default execute `main()` when loaded as a script
 if __name__ == "__main__":
