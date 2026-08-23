a607b8c0759659736c5df99426a2d4529e6630e2
Evert Vorster <evorster@gmail.com>
2026-08-23 10:02:31 +0200

    bases: convert text to ImGuiText #cR:G:B# color format
    Replace legacy hex '#RRGGBB' color codes in base room/mission/credits text with
    the ImGuiText '#cR:G:B#' format (the single canonical format). '#000000' (reset
    to default) becomes '#-c' (pop color). No visual change expected; unifies the
    text markup so TextPlane can be migrated onto ImGuiText without a hex parser.

--- bases/fixers/explore_delta_prime.py
+++ bases/fixers/explore_delta_prime.py
@@ -9,7 +9,7 @@ if VS.numActiveMissions()<=1:
         quest.removeQuest(plyr,'gemini_sector/gamma_navpoint',2)
     Base.Message('''Congratulations on your third mission! We have decided to go to the next system.
 Based on the data that you have collected, it appears that the other jump point should go
-to a system that we have named #55ffffdelta prime#000000. Again, collect info
+to a system that we have named #c.333:1:1#delta prime#-c. Again, collect info
 on six objects there and report back to me with your info.  You will earn 25000 credits.''')
     VS.LoadMission('patrol/explore_delta_prime.mission')
 else:
--- bases/main_menu.py
+++ bases/main_menu.py
@@ -52,7 +52,7 @@ credits_title = """\t=== Vega Strike 0.10.0 ===
 \t        ---Credits---"""
 
 credits_text_col1 = """
-#ff9999Developers:
+#c1:.6:.6#Developers:
 * Daniel Horn (Hellcatv)
 * Patrick Horn (Ace123)
 * Jack Sampson
@@ -67,7 +67,7 @@ credits_text_col1 = """
 * Alan Shieh
 * Spiritplumber
 
-#3399ffArtists:
+#c.2:.6:1#Artists:
 * James Carthew
 * Julien Chateau
 * Chuck_starchaser
@@ -86,7 +86,7 @@ credits_text_col1 = """
 
 
 credits_text_col2 = """
-#3399ffArtists (cont):
+#c.2:.6:1#Artists (cont):
 * Oblivion
 * Kinnear Penman
 * Phlogios
@@ -99,7 +99,7 @@ credits_text_col2 = """
 * Balint Szilard
 * Etheral Walker
 
-#ff99ffMusic:
+#c1:.6:1#Music:
 * Mike Ducharme
 * Falik
 * Peter Griffin
@@ -107,11 +107,11 @@ credits_text_col2 = """
 * Ken Suguro
 * Zaydana
 
-#ffcc33Web Site Design:
+#c1:.8:.2#Web Site Design:
 * Matthew Kruer
 * Aazelone Pyoleri (Zaydana)
 
-#99ff99Packaging:
+#c.6:1:.6#Packaging:
 * Vincent Fourmond
 * Mike Furr
 * Krister Kjellstroem
@@ -121,7 +121,7 @@ credits_text_col2 = """
 
 
 credits_text_col3 = """
-#99ffffCommunity:
+#c.6:1:1#Community:
 * Martin Baldwin
 * Bgaskey
 * Dilloh
@@ -142,7 +142,7 @@ credits_text_col3 = """
 * Wolphin
 * www2
 
-#66ccffAcknowledgements and thanks:
+#c.4:.8:1#Acknowledgements and thanks:
 Sourceforge
 Crystal Space
 gimp
@@ -151,7 +151,7 @@ NVidia (dds tools)
 Open standards
 Freedom of speech
 User patience
-#CCCCff... and any we forgot to mention :)
+#c.8:.8:1#... and any we forgot to mention :)
 """
 
 intro_title = """\t=== Vega Strike 0.10.0 ===
